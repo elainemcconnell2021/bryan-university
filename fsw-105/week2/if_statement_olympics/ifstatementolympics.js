54574a6aeb2eacbf2136e52caa5ba3d36609f270
elainemcconnell2021 <elaine.mcconnell@bryanuniversity.edu>
2019-12-17 21:28:02 -0500

    Commit on 12 17 19

---
+++ fsw-105/week2/if_statement_olympics/ifstatementolympics.js
@@ -0,0 +1,16 @@
+if (5 > 3) {
+    console.log ("is greater than")
+}
+
+
+var cat = 3
+if (cat == 3) {
+    console.log ("Is the length")
+}
+
+
+
+if ("cat" == "dog"){
+    console.log ("They are the same.")
+}
+else {console.log ("They are not the same.")}
